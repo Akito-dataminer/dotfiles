d306a7e2af6aaf66d53a9434f99a178aebc948ea
Akito Dataminer <akito.dataminer@gmail.com>
2023-05-03 15:22:05 +0900

    add - install_nvim.sh

---
+++ install_nvim.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+####################
+# install Neovim
+####################
+read -ep "directory you want to install nvim.appimage: " NVIM_DIST
+
+NVIM_DIST="${NVIM_DIST/#\~/$HOME}"
+
+if [ ! -d $NVIM_DIST ]; then
+  mkdir -p $NVIM_DIST
+fi
+
+# reference: https://github.com/neovim/neovim/wiki/Installing-Neovim#appimage-universal-linux-package
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+chmod u+x nvim.appimage
+mv nvim.appimage $NVIM_DIST
+ln -snvf $NVIM_DIST nvim.appimage /usr/local/bin/nvim
+
+####################
+# install deno
+####################
+curl -fsSL https://deno.land/x/install/install.sh | sh
+
+echo -e "export DENO_INSTALL=\"$HOME/.deno/\"\nexport PATH=\"\$DENO_INSTALL/bin:\$PATH\"" >> .profile
