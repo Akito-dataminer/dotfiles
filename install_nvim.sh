a98e6996d44b1b6b0853240667f40cc9e793a677
Akito Dataminer <akito.dataminer@gmail.com>
2023-05-03 15:40:11 +0900

    add - install commands for required files

--- install_nvim.sh
+++ install_nvim.sh
@@ -17,9 +17,13 @@ chmod u+x nvim.appimage
 mv nvim.appimage $NVIM_DIST
 ln -snvf $NVIM_DIST nvim.appimage /usr/local/bin/nvim
 
+sudo apt install libfuse-dev
+
 ####################
 # install deno
 ####################
+sudo apt install unzip # unzip is needed by script to install deno.
+
 curl -fsSL https://deno.land/x/install/install.sh | sh
 
 echo -e "export DENO_INSTALL=\"$HOME/.deno/\"\nexport PATH=\"\$DENO_INSTALL/bin:\$PATH\"" >> .profile
