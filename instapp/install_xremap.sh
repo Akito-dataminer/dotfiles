76d60205dc0ec844cf8a50bd06f638afd2b55104
Akito Dataminer <akito.dataminer@gmail.com>
2023-05-29 22:55:36 +0900

    add - script and the config files for xremap

--- instapp/install_xremap.sh
+++ instapp/install_xremap.sh
@@ -5,15 +5,26 @@ is_exist() {
   echo $?
 }
 
+function ask_yes_or_no() {
+  read -p "$1" YES_OR_NO
+
+  if [[ $YES_OR_NO == 'y' || $YES_OR_NO == 'Y' || $YES_OR_NO == "yes" ]]; then
+    return 1
+  fi
+
+  return 0
+}
+
 ####################
 # install jq to install xremap, if it is not exist
 ####################
 JQ_EXIST=`is_exist jq`
 
-if [ ${JQ_EXIST} -ne 0 ] ; then
+if [ ${JQ_EXIST} -ne 0 ]; then
+  echo jq is not installed.
   echo install jq
   sudo apt install -y jq
-  echo the install has been finished.
+  echo the installation has been finished.
 fi
 
 ####################
@@ -37,7 +48,6 @@ done
 
 read -p "which asset do you want to install? - " ORDERED_INDEX
 
-# errorが起きている。
 while [ 1 ]; do
   if [[ "$ORDERED_INDEX" =~ ^[0-9]+$ && $((ORDERED_INDEX)) -lt $((ASSET_NUM)) ]]; then break; fi
 
@@ -45,4 +55,24 @@ while [ 1 ]; do
   read -p "which asset do you want to install? - " ORDERED_INDEX
 done
 
-wget ${ASSET_URI_ARRAY[${ORDERED_INDEX}]}
+INSTALL_URI=${ASSET_URI_ARRAY[${ORDERED_INDEX}]}
+
+wget ${INSTALL_URI}
+unzip ${INSTALL_URI##*/}
+
+sudo chmod 750 xremap
+
+####################
+# config xremap( copy config files for xremap to some directries )
+####################
+sudo mkdir -p /usr/local/etc/xremap
+sudo mv .xremap/config.yml /usr/local/etc/xremap/config.yml
+
+sudo mv xremap /usr/local/bin/
+sudo systemctl enable xremap
+
+ask_yes_or_no "Do you want to start xremap now? [y]es/[n]o: "
+
+if [[ $? == 1 ]]; then
+  sudo systemctl start xremap
+fi
