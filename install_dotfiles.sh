1c7f6a78d6f55713cab0086a5dc96c6a3f98a261
Akito Dataminer <akito.dataminer@gmail.com>
2023-06-01 01:04:02 +0900

    bugfix - not to create link to .gitignore

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -6,6 +6,7 @@ cd $DOTFILES_ROOT
 for f in .??*; do
   [ "$f" = ".git" ] && continue
   [ "$f" = ".gitmodules" ] && continue
+  [ "$f" = ".gitignore" ] && continue
 
   ln -snfv ~/dotfiles/"$f" ~/
 done
