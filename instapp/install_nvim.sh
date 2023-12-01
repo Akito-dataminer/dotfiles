1ebd41a26bb91a4d74805fbedac193a0975f18f9
Akito Dataminer <akito.dataminer@gmail.com>
2023-12-01 22:09:19 +0900

    modify - the install destination to /usr/local/bin

--- instapp/install_nvim.sh
+++ instapp/install_nvim.sh
@@ -3,19 +3,9 @@
 ####################
 # install Neovim
 ####################
-read -ep "directory you want to install nvim.appimage: " NVIM_DIST
-
-NVIM_DIST="${NVIM_DIST/#\~/$HOME}"
-
-if [ ! -d $NVIM_DIST ]; then
-  mkdir -p $NVIM_DIST
-fi
-
-# reference: https://github.com/neovim/neovim/wiki/Installing-Neovim#appimage-universal-linux-package
-curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage # ref: https://github.com/neovim/neovim/wiki/Installing-Neovim#appimage-universal-linux-package
 chmod u+x nvim.appimage
-mv nvim.appimage $NVIM_DIST
-sudo ln -sf $NVIM_DIST/nvim.appimage /usr/local/bin/nvim
+mv nvim.appimage /usr/local/bin/nvim
 
 sudo apt install libfuse-dev
 
