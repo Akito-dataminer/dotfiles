c1f0807a321a71fd66b3e542c51f45fee6dc1bb8
Akito <102844371+Akito-dataminer@users.noreply.github.com>
2024-02-21 00:46:20 +0900

    Modify/Neovim installation method (#1)
    * modify - apt to apt-get and add -y option for them
    
    * modify - switch Neovim installation method to use stable release (from appimage)
    
    * remove - v option from tar

--- instapp/install_nvim.sh
+++ instapp/install_nvim.sh
@@ -3,16 +3,21 @@
 ####################
 # install Neovim
 ####################
-curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage # ref: https://github.com/neovim/neovim/wiki/Installing-Neovim#appimage-universal-linux-package
-chmod u+x nvim.appimage
-mv nvim.appimage /usr/local/bin/nvim
+curl -LO https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.tar.gz
 
-sudo apt install libfuse-dev
+tar -zxf nvim-linux64.tar.gz
+mv nvim-linux64/bin/nvim /usr/bin/nvim
+mv nvim-linux64/lib/nvim /usr/lib/nvim
+mv nvim-linux64/share/nvim/ /usr/share/nvim
+rm -rf nvim-linux64
+rm nvim-linux64.tar.gz
+
+sudo apt-get install -y libfuse-dev
 
 ####################
 # install deno
 ####################
-sudo apt install unzip # unzip is needed by script to install deno.
+sudo apt-get install -y unzip # unzip is needed by script to install deno.
 
 curl -fsSL https://deno.land/x/install/install.sh | sh
 
@@ -23,12 +28,12 @@ ln -snfv ~/dotfiles/.profile ~/
 # install npm
 # Reference: https://www.softel.co.jp/blogs/tech/archives/6487
 ####################
-sudo apt -y install nodejs npm
+sudo apt-get -y install nodejs npm
 
 sudo npm install -g n
 sudo n stable
-sudo apt purge -y nodejs npm
-sudo apt autoremove -y
+sudo apt-get purge -y nodejs npm
+sudo apt-get autoremove -y
 
 echo installed npm version: $(npm -v)
 echo installed node.js version: $(node -v)
